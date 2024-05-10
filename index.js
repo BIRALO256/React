e0b0c1c202c03839ed1196704ebd7d1a38c22f11
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-05-10 13:04:42 +0300

    intiall satages of leraning react

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-ReactDom.render(<h1>hello from react world</h1>,document.getElementById("root"))
+ReactDOM.render(
+    <div>
+        <h2>my first time writing react</h2>
+        <p>hello world</p>
+    </div>,document.getElementById("root"))
+
+ReactDOM.render(<ul><li>one</li><li>two</li></ul>,document.getElementById("list"))
+
+function MainContent(){
+    return (
+        <h1>am leraning react !</h1>
+    )
+}
+
+ReactDOM.render(
+    <MainContent/> ,document.getElementById("function")
+)
+
+
+console.log(MainContent)
